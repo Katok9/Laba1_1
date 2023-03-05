90208104cdc66df8d16d1547b63c86d0c22b25f0
Катерина Шевченко <katya14shevchenko@gmail.com>
2023-03-05 19:33:05 +0200

    Добавьте файлы проекта.

---
+++ Laba1_1/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Laba1_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "files/file1.txt";
+            List<Char> symbols = ReadFile(path);
+            Find(symbols);
+        }
+
+        static List<Char> ReadFile(string path)
+        {
+            string line;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                line = reader.ReadToEnd();
+            }
+
+            char[] symbols = line.ToCharArray();
+            List<Char> list = new List<Char>(symbols);
+
+            return list;
+        }
+
+        static void Find(List<Char> symbols)
+        {
+            char[] golosni = { 'а', 'о', 'у', 'е', 'и' };
+
+            int countGolosni = 0;
+            int countPrugolosni = 0;
+
+            foreach (char letter in symbols)
+            {
+                bool isLetter = Char.IsLetter(letter);
+
+                if (isLetter == false)
+                {
+                    continue;
+                }
+
+                char lowerLetter = Char.ToLower(letter);
+
+
+                if (golosni.Contains(lowerLetter))
+                {
+                    countGolosni++;
+                }
+                else
+                {
+                    countPrugolosni++;
+                }
+            }
+            Console.WriteLine("Кількість голосних: " + countGolosni + "\n" + "Кількість приголосних: " + countPrugolosni);
+        }
+    }
+}
